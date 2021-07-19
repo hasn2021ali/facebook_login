d7933056a70296029328b37dfdad7728f51c4c53
Hasan Ali <hasn2015ali@gmail.com>
2021-07-19 17:17:11 +0300

    add login , regiseter , save places to db

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,12 @@
 import os
 
 import flask
+from flask import request, jsonify, render_template , session, redirect, url_for
+from flask import json
 import requests_oauthlib
 from requests_oauthlib.compliance_fixes import facebook_compliance_fix
+import sqlite3
+
 
 # Your ngrok url, obtained after running "ngrok http 5000"
 URL = "https://hasanali101.pythonanywhere.com"
@@ -21,7 +25,64 @@ os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 app = flask.Flask(__name__)
 
+@app.route("/db")
+def db():
+    conn = sqlite3.connect("users.db")
+    return "Database opened successfully"
+    
+@app.route("/register", methods=["POSt"])
+def register_account():
+    name =  request.form.get('name')
+    email =  request.form.get('email')
+    password =  request.form.get('password')
 
+    try :
+         with sqlite3.connect("users.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO oauth_users (name,email,password)  VALUES (?,?,?)",(name,email,password) )
+            con.commit()
+            msg = "Record successfully added"
+    except:
+         con.rollback()
+         msg = "error in insert operation"
+         return msg
+      
+    finally:
+         
+         con.close()
+    return "register successfully"
+    
+ 
+    
+@app.route("/login", methods=["POSt"])
+def user_login():
+   
+    email =  request.form.get('email')
+    password =  request.form.get('password')
+
+    try :
+         with sqlite3.connect("users.db") as con:
+            cur = con.cursor()
+            cur.execute("select * from oauth_users  where email = '"+str(email)+"' ans password='"+str(password)+"'"  )
+            rows = cur.fetchall()
+            return jsonify(rows)
+            if len(rows) > 0 :
+                return jsonify(rows[0])
+            
+            return "no data"
+            
+            
+    except:
+         con.rollback()
+         msg = "error in login operation"
+         return msg
+      
+    finally:
+         
+         con.close()
+    return "not login"
+    
+ 
 @app.route("/")
 def index():
     return """
@@ -60,17 +121,51 @@ def callback():
         "https://graph.facebook.com/me?fields=id,name,email,posts{place},picture{url}"
     ).json()
     places = []
+    user_id = facebook_user_data["id"]
     email = facebook_user_data["email"]
     name = facebook_user_data["name"]
     posts = facebook_user_data["posts"]["data"]
-    for place in posts :
-        if place.__contains__("place") : 
-            places.append(place) 
+    
 
     picture_url = facebook_user_data.get("picture", {}).get("data", {}).get("url")
+    user_id =0
+    try :
+         with sqlite3.connect("users.db") as con:
+            cur = con.cursor()
+            # = (Int64)Command.ExecuteScalar();
+            cur.Execute("INSERT INTO oauth_users (name,email,password)  VALUES (?,?,?)",(name,email,"123456") )
+            user_id = cur.lastrowid
+            con.commit()
+
+            msg = "Record successfully added"
+    except:
+         con.rollback()
+         msg = "error in insert operation"
+         return msg
+      
+    finally:
+         
+         con.close()
+    for place in posts :
+        if place.__contains__("place") :
+            try :
+                with sqlite3.connect("users.db") as con:
+                    cur = con.cursor()
+                    # = (Int64)Command.ExecuteScalar();
+                    cur.Execute("INSERT INTO user_places (place_id,name,lat,lng, user_id ) VALUES (?,?,?,?,?)",(place["place"]["id"],place["place"]["name"],place["place"]["latitude"],place["place"]["longitude"] , user_id) )
+                    
+                    con.commit()
+
+                    msg = "Record successfully added"
+            except:
+                con.rollback()
+                msg = "error in insert operation"
+                return msg
+            places.append(place) 
 
     return f"""
     User information: <br>
+    User ID: {user_id} <br>
     Name: {name} <br>
     Email: {email} <br>
     Places: {len(places)} <br>
